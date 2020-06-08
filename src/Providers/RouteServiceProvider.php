6fb0dfd2a837d344f8117fbaa2f168efef6016e3
Mohamed Ibrahem <mohamed.ibr1994@gmail.com>
2020-06-08 12:41:47 +0300

    add api and web routes

---
+++ src/Providers/RouteServiceProvider.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Microboard\Providers;
+
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Route;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * This namespace is applied to your controller routes.
+     *
+     * In addition, it is set as the URL generator's root namespace.
+     *
+     * @var string
+     */
+    protected $namespace = 'Microboard\Http\Controllers';
+
+    /**
+     * The path to the "home" route for your application.
+     *
+     * @var string
+     */
+    public const HOME = '/';
+
+    /**
+     * Define the routes for the application.
+     *
+     * @return void
+     */
+    public function map()
+    {
+        $this->mapApiRoutes();
+        $this->mapWebRoutes();
+    }
+
+    /**
+     * Define the "web" routes for the application.
+     *
+     * These routes all receive session state, CSRF protection, etc.
+     *
+     * @return void
+     */
+    protected function mapWebRoutes()
+    {
+        Route::middleware('web')
+            ->prefix(config('microboard.routes.prefix', 'admin'))
+            ->middleware(config('microboard.routes.middleware', []))
+            ->name('microboard')
+            ->namespace($this->namespace)
+            ->group(__DIR__ . '/../../routes/web.php');
+    }
+
+    /**
+     * Define the "api" routes for the application.
+     *
+     * These routes are typically stateless.
+     *
+     * @return void
+     */
+    protected function mapApiRoutes()
+    {
+        Route::prefix(config('microboard.routes.prefix', 'admin') . '/api')
+            ->middleware(config('microboard.routes.apiMiddleware', []))
+            ->name('microboard.api')
+            ->namespace($this->namespace)
+            ->group(__DIR__ . '/../../routes/api.php');
+    }
+}
