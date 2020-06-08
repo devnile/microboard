58085f9f6678cac5a0af578bc41371ba693d3d10
Mohamed Ibrahem <mohamed.ibr1994@gmail.com>
2020-06-08 12:42:33 +0300

    configure routes attributes

--- config/config.php
+++ config/config.php
@@ -1,5 +1,17 @@
 <?php
 
 return [
+    /**
+     |
+     */
+    'routes' => [
+        'prefix' => 'admin',
+        'middleware' => [
+            'web',
+            // 'auth', 'can:view-dashboard'
+        ],
+        'apiMiddleware' => [
+            'api'
+        ]
     ]
 ];
