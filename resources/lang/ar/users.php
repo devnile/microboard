892b1e08541e9e06369f115b69e9e469064b15dc
Mohamed Ibrahem <mohamed.ibr1994@gmail.com>
2020-06-08 21:55:01 +0300

    translate auth pages

---
+++ resources/lang/ar/users.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'resource' => 'المستخدمين',
+    'fields' => [
+        'name' => 'الاسم',
+        'email' => 'البريد الإلكتروني',
+        'password' => 'كلمة المرور',
+        'password_confirmation' => 'تأكيد كلمة المرور'
+    ]
+];
