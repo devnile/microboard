a2fc60829f1e9898973605008cb58f0332b0b3e6
Mohamed Ibrahem <mohamed.ibr1994@gmail.com>
2020-06-08 12:53:34 +0300

    separate providers

---
+++ src/Providers/ViewServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Microboard\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ViewServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadTranslationsFrom(__DIR__ . '/../../resources/lang', 'microboard');
+        $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'microboard');
+
+        if ($this->app->runningInConsole()) {
+            /*$this->publishes([
+                __DIR__.'/../resources/views' => resource_path('views/vendor/microboard'),
+            ], 'views');*/
+
+            /*$this->publishes([
+                __DIR__.'/../resources/assets' => public_path('vendor/microboard'),
+            ], 'assets');*/
+
+            /*$this->publishes([
+                __DIR__.'/../resources/lang' => resource_path('lang/vendor/microboard'),
+            ], 'lang');*/
+        }
+    }
+}
