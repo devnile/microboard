862aa6603462d8b180867b6a8fe7a22158258e6a
Mohamed Ibrahem <mohamed.ibr1994@gmail.com>
2020-06-08 12:44:25 +0300

    add Install command

---
+++ src/Commands/InstallCommand.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace Microboard\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Validator;
+use Facades\Microboard\Factory;
+use Microboard\Models\Role;
+use Microboard\Models\User;
+
+class InstallCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'microboard:install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'It\'s publish the package assets, and migrate the database.';
+
+    /**
+     * The created admin.
+     *
+     * @var User
+     */
+    private $admin = null;
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->output->progressStart(4);
+        $this->output->newLine();
+
+        // Publish the assets (config, routes file, css and js files, database migrations)
+        $this->publishAssets();
+        $this->output->progressAdvance();
+        $this->output->newLine();
+
+        // Migrate the database
+        $this->migrateDatabase();
+        $this->output->progressAdvance();
+        $this->output->newLine();
+
+        // Create admin
+        if ($this->confirm('Create new admin?', false)) {
+            $this->askToCreateAdmin();
+        }
+        $this->output->progressAdvance();
+        $this->output->newLine();
+
+        // Create Permissions and roles
+        $this->createRolesAndPermissions();
+        $this->output->progressAdvance();
+        $this->output->newLine();
+
+        $this->info('All done.');
+        $this->info('Do something awesome!');
+    }
+
+    private function publishAssets()
+    {
+        $this->call('vendor:publish', [
+            '--provider' => 'Microboard\\MicroboardServiceProvider',
+        ]);
+        $this->info('All assets has been published');
+    }
+
+    private function migrateDatabase()
+    {
+        $this->call('migrate');
+        $this->info('Database has been migrated');
+    }
+
+    private function askToCreateAdmin()
+    {
+        $name = $this->ask('What is the admin name?');
+        $email = $this->ask('What is email?');
+        $password = $this->secret('What is password?');
+        $passwordConfirmation = $this->secret('Please confirm the password');
+
+        $validator = Validator::make([
+            'name' => $name,
+            'email' => $email,
+            'password' => $password,
+            'password_confirmation' => $passwordConfirmation
+        ], [
+            'name' => ['required', 'string', 'min:2', 'max:200'],
+            'email' => ['required', 'string', 'email', 'unique:users,email'],
+            'password' => ['required', 'string', 'min:4', 'max:200', 'confirmed'],
+        ]);
+
+        if ($validator->fails()) {
+            $this->error('Admin not created, See error messages below');
+            $errors = [];
+
+            foreach (['name', 'email', 'password'] as $field) {
+                if ($validator->errors()->has($field)) {
+                    $errors[] = [ucfirst($field), $validator->errors()->first($field)];
+                }
+            }
+
+            $this->output->table(['Field', 'Message'], $errors);
+
+            return $this->askToCreateAdmin();
+        }
+
+        $this->admin = User::create([
+            'name' => $name,
+            'email' => $email,
+            'password' => bcrypt($password)
+        ]);
+
+        $this->info('Admin creates successfully');
+    }
+
+    private function createRolesAndPermissions()
+    {
+        $adminRole = Role::updateOrCreate(['name' => 'admin', 'display_name' => 'Administrator']);
+        Role::updateOrCreate(['name' => 'user', 'display_name' => 'Normal user']);
+
+        $this->info('Default roles has created/updated successfully');
+
+        $this->info('Default permissions has created/updated successfully');
+
+        if ($this->admin) {
+            $adminRole->users()->save($this->admin);
+        }
+    }
+}
---
+++ tests/InstallCommandTest.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Microboard\Tests;
+
+use Illuminate\Support\Facades\Schema;
+
+class InstallCommandTest extends TestCase
+{
+    protected function install()
+    {
+        return $this->artisan('microboard:install')
+            ->expectsConfirmation('Create new admin?', 'no');
+    }
+
+    protected function installWithAdmin() {
+        return $this->artisan('microboard:install')
+            ->expectsConfirmation('Create new admin?', 'yes')
+            ->expectsQuestion('What is the admin name?', 'admin')
+            ->expectsQuestion('What is email?', 'admin@system.app')
+            ->expectsQuestion('What is password?', 'admin')
+            ->expectsQuestion('Please confirm the password', 'admin');
+    }
+
+    /** @test * */
+    public function it_publish_the_assets_files()
+    {
+        $this->install()->expectsOutput('All assets has been published');
+    }
+
+    /** @test * */
+    public function it_migrates_the_package_migrations()
+    {
+        $this->install()->expectsOutput('Database has been migrated');
+
+        $this->assertTrue(Schema::hasTable('settings'));
+        $this->assertTrue(Schema::hasTable('roles'));
+        $this->assertTrue(Schema::hasTable('permissions'));
+        $this->assertTrue(Schema::hasTable('permission_role'));
+        $this->assertTrue(Schema::hasColumns('users', [
+            'avatar', 'role_id'
+        ]));
+    }
+
+    /** @test * */
+    public function it_creates_admin_and_default_roles_and_permissions()
+    {
+        $this->installWithAdmin()->expectsOutput('Admin creates successfully');
+
+        $this->assertDatabaseCount('users', 1);
+        $this->assertDatabaseHas('users', [
+            'name' => 'admin',
+            'email' => 'admin@system.app'
+        ]);
+    }
+
+    /** @test * */
+    public function it_cannot_create_new_admin_without_valid_data()
+    {
+        $command = $this->artisan('microboard:install');
+
+        $command->expectsConfirmation('Create new admin?', 'yes')
+            ->expectsQuestion('What is the admin name?', 'admin')
+            ->expectsQuestion('What is email?', 'admin@system.app')
+            ->expectsQuestion('What is password?', 'admin')
+            ->expectsQuestion('Please confirm the password', 'not match')
+            ->expectsOutput('Admin not created, See error messages below');
+
+        $this->assertDatabaseCount('users', 0);
+
+        $command->expectsQuestion('What is the admin name?', 'admin')
+            ->expectsQuestion('What is email?', 'admin@system.app')
+            ->expectsQuestion('What is password?', 'admin')
+            ->expectsQuestion('Please confirm the password', 'admin')
+            ->expectsOutput('Admin creates successfully');
+    }
+
+    /** @test * */
+    public function it_creates_default_roles_and_permissions()
+    {
+        $this->installWithAdmin()
+            ->expectsOutput('Default roles has created/updated successfully')
+            ->expectsOutput('Default permissions has created/updated successfully');
+
+        $this->assertDatabaseHas('roles', ['name' => 'admin']);
+        $this->assertDatabaseHas('roles', ['name' => 'user']);
+    }
+}
