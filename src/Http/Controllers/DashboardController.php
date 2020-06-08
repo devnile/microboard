a4852abd30ff565c60b038214b97bc98a764322e
Mohamed Ibrahem <mohamed.ibr1994@gmail.com>
2020-06-08 18:23:33 +0300

    add DashboardController

---
+++ src/Http/Controllers/DashboardController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Microboard\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class DashboardController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param Request $request
+     * @return View
+     */
+    public function __invoke(Request $request)
+    {
+        return view('microboard::index');
+    }
+}
