c089e9ffdef9bbf12b022eef1ad7ccc2cf9c1cbd
Mohamed Ibrahem <mohamed.ibr1994@gmail.com>
2020-06-10 13:04:00 +0300

    Update breadcrumb.blade.php

--- resources/views/layout/partials/breadcrumb.blade.php
+++ resources/views/layout/partials/breadcrumb.blade.php
@@ -1,4 +1,4 @@
-<nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
+<nav aria-label="breadcrumb" class="d-none d-md-inline-block{{ config('microboard.view.enable_global_search') ? ' ml-md-4' : '' }}">
     <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
         <li class="breadcrumb-item"><a href="{{ route('microboard.home') }}"><i class="fas fa-home"></i></a></li>
         @isset($breadcrumbs)
