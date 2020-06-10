fbd6002df59bbf5fe6603f83c820c04538a97102
Mohamed Ibrahem <mohamed.ibr1994@gmail.com>
2020-06-10 11:23:56 +0300

    Create index view

---
+++ resources/views/resource/index.blade.php
@@ -0,0 +1,36 @@
+@extends('microboard::layout.app', [
+    'breadcrumbs' => [
+        ['name' => 'Resource']
+    ]
+])
+
+@section('title', 'Resource page')
+
+@section('actions')
+    <a href="#" class="btn btn-sm btn-neutral">
+        New resource
+    </a>
+@endsection
+
+@section('content')
+    <div class="row">
+        <div class="col">
+            {!! $dataTable->table() !!}
+        </div>
+    </div>
+@endsection
+
+@push('styles')
+    <link rel="stylesheet" href="{{ asset('vendor/microboard/vendor/datatables.net/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/microboard/vendor/datatables.net/buttons.bootstrap4.min.css') }}">
+@endpush
+
+@push('scripts')
+    <script src="{{ asset('vendor/microboard/vendor/datatables.net/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('vendor/microboard/vendor/datatables.net/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('vendor/microboard/vendor/datatables.net/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset('vendor/microboard/vendor/datatables.net/buttons.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('vendor/datatables/buttons.server-side.js') }}"></script>
+
+    {!! $dataTable->scripts() !!}
+@endpush
