8cf62bbfc8512b100b944e316b7eac095b3ecf4b
Mohamed Ibrahem <mohamed.ibr1994@gmail.com>
2020-06-10 13:08:50 +0300

    translate to English

---
+++ resources/lang/en/datatable.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'search' => '<span>I\'m searching about...</span>',
+    'lengthMenu' => '<i class="fa fa-filter mr-3"></i> _MENU_',
+    'paginate' => [
+        'first' => '<i class="fas fa-angle-double-left"></i>',
+        'previous' => '<i class="fas fa-angle-left"></i>',
+        'next' => '<i class="fas fa-angle-right"></i>',
+        'last' => '<i class="fas fa-angle-double-right"></i>'
+    ],
+    'print' => 'Print',
+    'excel' => 'Export to Excel',
+    'reload' => 'Reload'
+];
