3a7cde55414d8f1bf14c4c65c6583093d9fff1ee
Mohamed Ibrahem <mohamed.ibr1994@gmail.com>
2020-06-10 12:13:24 +0300

    update table actions to reach the model

--- src/DataTables/UserDataTable.php
+++ src/DataTables/UserDataTable.php
@@ -19,7 +19,38 @@ class UserDataTable extends DataTable
     {
         return datatables()
             ->eloquent($query)
-            ->addColumn('action', 'microboard::layout.partials.data-table-actions');
+            ->addColumn('action', function (User $user) {
+                $html = '';
+
+                if (true) {
+                    $html .= '<a href="' . route('microboard.users.show', $user) . '" ' .
+                        'class="table-action" data-toggle="tooltip" ' .
+                        'data-original-title="' . trans('microboard::users.view.action-button') . '">' .
+                        '<i class="fas fa-eye"></i></a>';
+                }
+
+                if (true) {
+                    $html .= '<a href="' . route('microboard.users.edit', $user) . '" ' .
+                        'class="table-action" data-toggle="tooltip" ' .
+                        'data-original-title="' . trans('microboard::users.edit.action-button') . '">' .
+                        '<i class="fas fa-edit"></i></a>';
+                }
+
+                if (true) {
+                    $html .= '<form action="' . route('microboard.users.destroy', $user) . '" method="post" class="d-inline-block">' .
+                        csrf_field() . method_field('DELETE') .
+                        '<button type="submit" data-toggle="tooltip" ' .
+                        'class="bg-transparent border-0 p-0 table-action table-action-delete" ' .
+                        'data-original-title="'. trans('microboard::users.delete.action-button') .'" ' .
+                        'data-modal-title="'. trans('microboard::users.delete.title') .'" ' .
+                        'data-modal-text="'. trans('microboard::users.delete.text') .'" ' .
+                        'data-confirm="'. trans('microboard::users.delete.confirm') .'" ' .
+                        'data-cancel="'. trans('microboard::users.delete.cancel') .'">' .
+                        '<i class="fas fa-trash"></i></button></form>';
+                }
+
+                return $html;
+            });
     }
 
     /**
@@ -50,13 +81,13 @@ class UserDataTable extends DataTable
             ->minifiedAjax()
             ->dom("<'card'" .
                 "<'card-header border-0'<'row align-items-center'<'col-12 col-sm-8 col-md-6'<'row no-gutters'<'col-4'l><'col-8'f>>><'col-12 col-sm-4 col-md-6 text-right'B>>>" .
-                "<'table-responsive't>".
-                "<'card-footer'<'row'<'col-12 col-sm-6'i><'col-12 col-sm-6'p>>>".
+                "<'table-responsive't>" .
+                "<'card-footer'<'row'<'col-12 col-sm-6'i><'col-12 col-sm-6'p>>>" .
                 "r>")
             ->buttons(
-                Button::make('print')->text('<span>'. trans('microboard::datatable.print') .'</span><i class="fa fa-print"></i>'),
-                Button::make('excel')->text('<span>'. trans('microboard::datatable.excel') .'</span><i class="fa fa-file-excel"></i>'),
-                Button::make('reload')->text('<span>'. trans('microboard::datatable.reload') .'</span><i class="fa fa-sync"></i>')
+                Button::make('print')->text('<span>' . trans('microboard::datatable.print') . '</span><i class="fa fa-print"></i>'),
+                Button::make('excel')->text('<span>' . trans('microboard::datatable.excel') . '</span><i class="fa fa-file-excel"></i>'),
+                Button::make('reload')->text('<span>' . trans('microboard::datatable.reload') . '</span><i class="fa fa-sync"></i>')
             );
     }
 
