9f214f37aaf987671fefac525ba99cb3140bd7ea
Mohamed Ibrahem <mohamed.ibr1994@gmail.com>
2020-06-12 00:29:02 +0300

    Update UpdateFormRequest.php

--- src/Http/Requests/User/UpdateFormRequest.php
+++ src/Http/Requests/User/UpdateFormRequest.php
@@ -27,12 +27,12 @@ class UpdateFormRequest extends FormRequest
         return [
             'name' => ['required', 'string'],
             'email' => ['required', 'string', 'unique:users,email,' . $this->user->id],
-            'auth_password' => ['required_with:password', 'string', function($attribute, $value, $fail) {
+            'auth_password' => ['nullable', 'required_with:password', 'string', function($attribute, $value, $fail) {
                 if (!Hash::check($value, auth()->user()->password)) {
                     $fail(trans('auth.failed'));
                 }
             }],
-            'password' => ['required_with:auth_password', 'string', 'min:8', 'confirmed'],
+            'password' => ['nullable', 'required_with:auth_password', 'string', 'min:8', 'confirmed'],
             'role_id' => ['required', 'string', 'exists:roles,id'],
         ];
     }
