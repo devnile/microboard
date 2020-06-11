ff3e3e4638fcfe7bb877c05fa1883c9d9b46e9fe
Mohamed Ibrahem <mohamed.ibr1994@gmail.com>
2020-06-11 22:19:59 +0300

    Update checkbox.blade.php

--- resources/views/checkbox.blade.php
+++ resources/views/checkbox.blade.php
@@ -1,23 +1,22 @@
 @php
     /** @var $name */
     /** @var $value */
+    /** @var $checked */
     /** @var \Illuminate\Support\ViewErrorBag $errors */
     /** @var \Illuminate\Support\Collection $attributes */
     $attributes = collect($attributes);
 
-    $title = $attributes->get('title', 'None');
+    $title = $attributes->get('title');
     $alternative = $attributes->get('alternative', false);
+    $id = $attributes->get('id', $name);
 
     $attributes = $attributes->merge([
-        'class' => 'custom-control-input'
+        'class' => 'custom-control-input',
     ])
     ->except(['title', 'alternative'])
     ->all();
 @endphp
 <div class="custom-control custom-checkbox{{ $alternative ? ' custom-control-alternative' : '' }}">
-    {!! Form::checkbox($name, true, $value, $attributes) !!}
-
-    @isset($title)
-        {!! Form::label($name, $title, ['class' => 'custom-control-label'], true) !!}
-    @endisset
+    {!! Form::checkbox($name, $value, $checked, $attributes) !!}
+    {!! Form::label($id, $title, ['class' => 'custom-control-label'], true) !!}
 </div>
