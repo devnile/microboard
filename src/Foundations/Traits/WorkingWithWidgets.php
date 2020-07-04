5b8799e1e5bbd45ba0c2864f80bb06cae06d5050
Mohamed Ibrahem <mohamed.ibr1994@gmail.com>
2020-07-04 23:46:51 +0200

    fix Class was composed issue

--- src/Foundations/Traits/WorkingWithWidgets.php
+++ src/Foundations/Traits/WorkingWithWidgets.php
@@ -5,41 +5,6 @@ namespace Microboard\Foundations\Traits;
 trait WorkingWithWidgets
 {
     /**
-     * Array of widgets that appear on index page.
-     *
-     * @var string[]
-     */
-    protected array $indexWidgets = [];
-
-    /**
-     * Array of widgets that appear on show page.
-     *
-     * @var string[]
-     */
-    protected $showWidgets = [];
-
-    /**
-     * Array of widgets that appear on create page.
-     *
-     * @var string[]
-     */
-    protected $createWidgets = [];
-
-    /**
-     * Array of widgets that appear on update page.
-     *
-     * @var string[]
-     */
-    protected $updateWidgets = [];
-
-    /**
-     * Array of widgets that appear on create and update.
-     *
-     * @var string[]
-     */
-    protected $formsWidgets = [];
-
-    /**
      * Get widgets for the specific view.
      *
      * @param $view
